dd554c0c412735402ca9dbc82b1dbc9fdd978baa
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-10-08 15:20:00 +0100

    Switch back to a single month for the manuscript.

--- R scripts/figure.8 and 9 STRESS AND DISTRUBANCE.R
+++ R scripts/figure.8 and 9 STRESS AND DISTRUBANCE.R
@@ -9,20 +9,27 @@ Packages <- c("tidyverse", "sf", "raster", "patchwork")                       # 
 lapply(Packages, library, character.only = TRUE)                              # Load packages
 source("./R scripts/@_Plotting rules.R")
 
+domain <- readRDS("./Objects/Domain.rds")
+
 proj <- rgdal::make_EPSG() %>%                                                # Get proj4 strings from epsg codes
   filter(code == crs)
 
+average <- brick("./Output/Greenland_and_barents_sea_seasonal_disturbance.nc") %>% # Open all layers
+  exactextractr::exact_extract(st_union(domain), fun = "sum") %>%             # Total the disturbance for each layer over the whole model domain
+  as.numeric()                                                                # Convert to numeric vector
+
+layer <- which(average == max(average))                                       # Which layer has the most stress?
+
 #### Stress ####
 
-stress <- brick("./Output/Greenland_and_barents_sea_seasonal_stress.nc") %>%
+stress <- raster("./Output/Greenland_and_barents_sea_seasonal_stress.nc", layer = layer) %>%
   projectRaster(crs = proj$prj4) %>% 
   as.data.frame(xy = T) %>% 
   drop_na() %>% 
-  pivot_longer(X1:X12, names_to = "Month", values_to = "Stress") %>% 
-  mutate(Month = month.name[as.numeric(str_sub(Month, start = 2))])
-stress$Month <- factor(stress$Month, levels = month.name)                      # Set facet order
+  mutate(Var = "Bed shear stress")
+names(stress) <- c("x", "y", "Stress", "Var")
 
-ggplot() + 
+S <- ggplot() + 
   ggpubr::background_image(png::readPNG("./Figures/background.png")) +
   geom_path(data = marks, aes(x=x, y=y, group = graticule), colour = "grey", size = 0.2) + # Add Graticules 
   geom_raster(data = stress, aes(x=x, y=y, fill = Stress)) +                   # Add rasters
@@ -30,28 +37,39 @@ ggplot() +
                               option = "B", trans = "log", 
                               breaks = c(0.0001, 0.001, 0.01, 0.1, 3)) +       # Specify fill
   sediment_aes +                                                               # Use consistent aesthetics
-  facet_wrap(vars(Month), ncol = 2) +                                          # Facet by month
+  facet_wrap(vars(Var)) +                                          # Facet by month
   NULL
-
-ggsave("./Figures/Figure 7 stress.png", width = 13, height = 20, units = "cm", dpi = 1500)
 
 #### Disturbance ####
 
-disturbance <- brick("./Output/Greenland_and_barents_sea_seasonal_disturbance.nc") %>%
+disturbance <- raster("./Output/Greenland_and_barents_sea_seasonal_disturbance.nc", layer = layer) %>%
   projectRaster(crs = proj$prj4) %>% 
   as.data.frame(xy = T) %>% 
   drop_na() %>% 
-  pivot_longer(X1:X12, names_to = "Month", values_to = "Disturbance") %>% 
-  mutate(Month = month.name[as.numeric(str_sub(Month, start = 2))])
-disturbance$Month <- factor(disturbance$Month, levels = month.name)                      # Set facet order
+  mutate(Var = "Natural Disturbance")
+names(disturbance) <- c("x", "y", "Disturbance", "Var")
+  
+#ggplot() +                                                                # Where could we limit the colour scale to make it more readable?
+#  geom_freqpoly(data = disturbance, aes(Disturbance*100, colour = Month)) # Chose 10%
 
-ggplot() + 
+D <- ggplot() + 
   ggpubr::background_image(png::readPNG("./Figures/background.png")) +
   geom_path(data = marks, aes(x=x, y=y, group = graticule), colour = "grey", size = 0.2) + # Add Graticules 
-  geom_raster(data = disturbance, aes(x=x, y=y, fill = Disturbance*100)) +                    # Add rasters
-  viridis::scale_fill_viridis(name = "Time disturbed (%)", trans = "sqrt", option = "B") +                      # Specify fill
+  geom_raster(data = filter(disturbance,Disturbance > 0), aes(x=x, y=y, fill = Disturbance*100)) +  # Add rasters
+  viridis::scale_fill_viridis(name = "Time disturbed (%)", breaks = c(2, 4, 6, 8, 10), 
+                              limits = c(0, 10), labels = c("2", "3", "4", "8", "10+"),
+                              option = "B",
+                              rescaler = function(x, to = c(0,1), from = NULL) {   # Rescale values larger than 10 to limit colour scale
+                                ifelse(x<10,
+                                       scales::rescale(x,
+                                                       to = to,
+                                                       from = c(min(x, na.rm = TRUE), 10)),
+                                       1)}) +                      # Limit max colour fill
   sediment_aes +                                                                           # Use consistent aesthetics
-  facet_wrap(vars(Month), ncol = 2) +                                                      # Facet by month
+  facet_wrap(vars(Var)) +                                          # Facet by month
   NULL
 
-ggsave("./Figures/Figure 8 disturbance.png", width = 13, height = 20, units = "cm", dpi = 1500)
+
+combined <- S/D
+
+ggsave("./Figures/Figure 7 movement.png", width = 13, height = 14, units = "cm", dpi = 1500)
