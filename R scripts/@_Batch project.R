98bd0a8a1917123826ef52cdb31af26ee99d4185
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-01-19 09:49:44 +0000

    docs: create a readme.

--- R scripts/@_Batch project.R
+++ R scripts/@_Batch project.R
@@ -1,42 +1,33 @@
 
 ## Run batches of R scripts. Handy if you want scripts to run after another finishes while you're away from the machine
-
-#### Set up ####
-
-packages <- c("tidyverse", "callr", "tictoc")           # List packages
-lapply(packages, library, character.only = TRUE)        # Load packages
-
-execute <- function(x) {
-  tic(x)
-  r(function(x) source(x), args = list(x), spinner = TRUE)
-  toc(log = T, quiet = T)
-
-  usethis::ui_done("{usethis::ui_field(x)} completed. {praise::praise('${Exclamation}!')}")  } # Run an R script in it's own R session and record how long it takes
-
-#### Batch process scripts ####
+library(tidyverse)
 
 scripts <- c(                                            # List scripts in the order you want to run them
 #  "./R scripts/sediment.00 DOMAINS.R",
-  "./R scripts/sediment.01 GRID WATER.R",
-  "./R scripts/sediment.02 CROP SINMOD.R", 
-  "./R scripts/sediment.03 CROP WAVES.R",
-  "./R scripts/sediment.04 WATER TS.R",
-  "./R scripts/sediment.05 INTERPOLATE WAVES.R",
-  "./R scripts/sediment.06 SHEARSTRESS.R",
-  "./R scripts/sediment.07 RF PREDICTORS.R",
-  "./R scripts/sediment.08 H2O.R",
-  "./R scripts/sediment.09 SEDIMENT FRACTIONS.R",
-  "./R scripts/sediment.10 WRITE CSV.R",
-  "./R scripts/sediment.11 WRITE NETCDF.R",
-  "./R scripts/sediment.12 SEASONAL STRESS.R",
-  "./R scripts/sediment.13 DISTURBANCE.R"
-#  "./R scripts/sediment.14 WRITE SEASONAL.R"
+  # "./R scripts/sediment.01 GRID WATER.R",
+  # "./R scripts/sediment.02 CROP SINMOD.R", 
+  # "./R scripts/sediment.03 CROP WAVES.R",
+  # "./R scripts/sediment.04 WATER TS.R",
+  # "./R scripts/sediment.05 INTERPOLATE WAVES.R",
+  # "./R scripts/sediment.06 EXPAND.R",
+  # "./R scripts/sediment.07 SHEARSTRESS.R",
+  # "./R scripts/sediment.08 RF PREDICTORS.R",
+  # "./R scripts/sediment.09 H2O.R",
+  # "./R scripts/sediment.10 SEDIMENT FRACTIONS.R",
+  # "./R scripts/sediment.11 WRITE CSV.R",
+  # "./R scripts/sediment.12 OMC.R",
+  # "./R scripts/sediment.13 PERM PORE.R",
+  # "./R scripts/sediment.14 WRITE NETCDF.R",
+  # "./R scripts/sediment.15 SEASONAL STRESS.R",
+  "./R scripts/sediment.16 DAILY DISTURBANCE.R",
+  "./R scripts/sediment.17 RASTERISE DISTURBANCE.R"
+
 ) %>% 
-  map(execute)
+  map(MiMeMo.tools::execute)
 
 #### Plot run times ####
 
-timings <- tic.log(format = F) %>%                                                     # Get the log of timings
+timings <- tictoc::tic.log(format = F) %>%                                                     # Get the log of timings
   lapply(function(x) data.frame("Script" = x$msg, Minutes = (x$toc - x$tic)/60)) %>%   # Get a dataframe of scripts and runtimes in minutes
   bind_rows() %>%                                                                      # Get a single dataframe
   separate(Script, into = c(NA, "Script"), sep = "/R scripts/") %>% 
