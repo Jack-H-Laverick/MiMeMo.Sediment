72bd7bbf9b9bb653200b978771ee68d16fb71a22
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-01-29 16:38:04 +0000

    fix: remove a ")" from file names and converted column object to character.

--- R scripts/sediment.17 RASTERISE DISTURBANCE.R
+++ R scripts/sediment.17 RASTERISE DISTURBANCE.R
@@ -6,7 +6,7 @@ rm(list=ls())                                                               # Wi
 packages <- c("MiMeMo.tools", "furrr", "feather", "raster")                 # List packages
 lapply(packages, library, character.only = TRUE)                            # Load packages
 
-plan(multiprocess)
+plan(multisession)
 
 grid <- readRDS("./Objects/Water look up table.rds") %>%                    # Import pixel meta-data
   dplyr::select(x = Longitude, y = Latitude) %>%                          
@@ -15,60 +15,68 @@ grid <- readRDS("./Objects/Water look up table.rds") %>%                    # Im
 
 chunks <- length(list.files("./Objects/disturbance_input/"))                # Get the number of cached files to combine again 
 
-columns <- data.frame(column = names(read_feather("./Objects/disturbance_input/daily_1.feather)"))) %>% # Get the column names in a file, we want one raster per column
+columns <- data.frame(column = names(read_feather("./Objects/disturbance_input/daily_1.feather"))) %>% # Get the column names in a file, we want one raster per column
   separate(column, into = c("Day", "Sediment_fraction"), remove =  F, sep = "_") %>% # Get the variable name out of the column
   group_split(Sediment_fraction)                                            # Split by variable so they each go into their own netcdf file
 
+check <- future_map(1:chunks, ~{read_feather(glue::glue("./Objects/disturbance_input/daily_{.x}.feather")) %>% 
+      dplyr::select(columns[[1]]$column)}, .progress =T) %>% 
+   data.table::rbindlist()  
+unique(check$`20030201_0.007826238`) # Does contain trues.
+
 #### Create a file of silt disturbance ####
 
-silt <- future_map(columns[[1]]$column, ~{                                  # For each column containing silt data
-  
-  column <- .x
-  # Read in all the chunks of data from cached files in the correct order
-  data <- map(1:chunks, ~{read_feather(glue::glue("./Objects/disturbance_input/daily_{.x}.feather)"), columns = column)}) %>% 
-                          data.table::rbindlist()  
+tictoc::tic()
+silt <- future_map(columns[[1]]$column, ~{
+   
+   column <- as.character(.x)
+   
+   data <- map(1:chunks, ~{read_feather(glue::glue("./Objects/disturbance_input/daily_{.x}.feather"), columns = column)}) %>% 
+      data.table::rbindlist()
 
-  raster <- mutate(grid, z = data) %>%                                      # Attach metadata
-    rasterFromXYZ(crs = "+proj=longlat +datum=WGS84")                       # Convert to a raster
-  }, .progress = T) %>% 
-  brick() %>%                                                               # Combine into a single cube
-  setNames(columns[[1]]$Day) %>% 
-  writeRaster(str_glue("./Output/Daily_disturbance_silt.nc"), overwrite = TRUE, format = "CDF", # Save out as netcdf.
-               varname= "Silt_ND", longname = "Is a pixel disturbed during a day", 
-               varunit = "logical", xname = "Longitude", yname = "Latitude", zname = "Day")
+   raster <- mutate(grid, z = data) %>%                                            # Attach metadata
+      rasterFromXYZ(crs = "+proj=longlat +datum=WGS84")
+   }, .progress = T) %>%                                                             # Convert to a raster
+brick() %>% 
+writeRaster(str_glue("./Output/Daily_disturbance_silt.nc"), overwrite = TRUE, 
+            format = "CDF", varname= "Silt_ND", longname = "Is a pixel disturbed during a day", 
+            varunit = "logical", xname = "Longitude", yname = "Latitude", zname = "Day")
+tictoc::toc()
 
 #### Create a file of sand disturbance ####
- 
+
+tictoc::tic()
 sand <- future_map(columns[[2]]$column, ~{
    
-   column <- .x
+   column <- as.character(.x)
    
-   data <- map(1:chunks, ~{read_feather(glue::glue("./Objects/disturbance_input/daily_{.x}.feather)"), columns = column)}) %>% 
-     data.table::rbindlist()  
+   data <- map(1:chunks, ~{read_feather(glue::glue("./Objects/disturbance_input/daily_{.x}.feather"), columns = column)}) %>% 
+      data.table::rbindlist()
    
-   raster <- mutate(grid, z = data) %>% 
-     rasterFromXYZ(crs = "+proj=longlat +datum=WGS84")
- }, .progress = T) %>% 
+   raster <- mutate(grid, z = data) %>%                                            # Attach metadata
+      rasterFromXYZ(crs = "+proj=longlat +datum=WGS84")
+}, .progress = T) %>%                                                             # Convert to a raster
    brick() %>% 
-   setNames(columns[[2]]$Day) %>% 
-   writeRaster(str_glue("./Output/Daily_disturbance_sand.nc"), overwrite = TRUE, format = "CDF", 
-               varname= "Sand_ND", longname = "Is a pixel disturbed during a day", 
-               varunit = "logical", xname = "Longitude", yname = "Latitude", zname = "Day")
+   writeRaster(str_glue("./Output/Daily_disturbance_sand.nc"), overwrite = TRUE, 
+            format = "CDF", varname= "Sand_ND", longname = "Is a pixel disturbed during a day", 
+            varunit = "logical", xname = "Longitude", yname = "Latitude", zname = "Day")
+tictoc::toc()
 
 #### Create a file of gravel disturbance ####
 
+tictoc::tic()
 gravel <- future_map(columns[[3]]$column, ~{
    
-   column <- .x
+   column <- as.character(.x)
    
-   data <- map(1:chunks, ~{read_feather(glue::glue("./Objects/disturbance_input/daily_{.x}.feather)"), columns = column)}) %>% 
-     data.table::rbindlist()  
+   data <- map(1:chunks, ~{read_feather(glue::glue("./Objects/disturbance_input/daily_{.x}.feather"), columns = column)}) %>% 
+      data.table::rbindlist()
    
-   raster <- mutate(grid, z = data) %>% 
-     rasterFromXYZ(crs = "+proj=longlat +datum=WGS84")
- }, .progress = T) %>% 
+   raster <- mutate(grid, z = data) %>%                                            # Attach metadata
+      rasterFromXYZ(crs = "+proj=longlat +datum=WGS84")
+}, .progress = T) %>%                                                             # Convert to a raster
    brick() %>% 
-   setNames(columns[[3]]$Day) %>% 
-   writeRaster(str_glue("./Output/Daily_disturbance_gravel.nc"), overwrite = TRUE, format = "CDF", 
-             varname= "Gravel_ND", longname = "Is a pixel disturbed during a day", 
-             varunit = "Disturbed", xname = "Longitude", yname = "Latitude", zname = "Day")
+writeRaster(str_glue("./Output/Daily_disturbance_gravel.nc"), overwrite = TRUE, 
+            format = "CDF", varname= "Gravel_ND", longname = "Is a pixel disturbed during a day", 
+            varunit = "logical", xname = "Longitude", yname = "Latitude", zname = "Day")
+tictoc::toc()
