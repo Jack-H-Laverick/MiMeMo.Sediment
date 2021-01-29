67ff9f4413dec1c5d10048b48118ef73a664bd18
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-01-29 16:35:14 +0000

    feat: speed up calculating d50s by adding it to the translation object, not calculating per pixel.

--- R scripts/sediment.10 SEDIMENT FRACTIONS.R
+++ R scripts/sediment.10 SEDIMENT FRACTIONS.R
@@ -5,32 +5,31 @@
 
 rm(list=ls())
 
-Packages <- c("MiMeMo.tools", "tidyverse", "sf", "rnaturalearth", "viridis", "patchwork", "tictoc") # List packages
-lapply(Packages, library, character.only = TRUE)                 # Load packages
+library(MiMeMo.tools) # List packages
 source("./R scripts/@_Set up file.R")
 
-domains <- readRDS("./Objects/Domain.rds") %>%                  # Load SF polygons of the MiMeMo model domains
+domains <- readRDS("./Objects/Domain.rds") %>%                   # Load SF polygons of the MiMeMo model domains
   st_transform(crs = 4326)                                       # Transform to Lat/Lon to match other objects
 
 Sediment <- readRDS("./Objects/modelled_sediment.rds")           # Import full sediment grid
 
 Translate <- read.csv("./Data/Sediment nominal values.csv") %>%  # Import quantitative values for classes
-  mutate(Sed_class = as.factor(Sed_class))
-
-world <- ne_countries(scale = "medium", returnclass = "sf")      # Get a world map
+  mutate(Sed_class = as.factor(Sed_class)) %>% 
+  rowwise() %>% 
+  mutate(D50 = 10^weighted.mean(c(log10(11.31371), log10(0.3535534), log10(0.007826238)), 
+                                c(Gravel,Sand,Silt))) %>%        # Weighted mean of the central value from grain size classes
+ungroup()
 
 #### Transform categorical to continuous ####
 
 Sed_quant <- left_join(Sediment, Translate) %>% 
   mutate(Sed_class = as.factor(Sed_class)) %>% 
-  sfc_as_cols() %>%                                              # For quicker plotting
-  mutate(D50 = ((Gravel * 2) + (Sand * 1.03125) + (Silt * 0.03174))/ 100) # Weighted mean of the central value from grain size classes
+  sfc_as_cols()                                                  # For quicker plotting
 
 ggplot() + 
-  geom_raster(data = Sed_quant, aes(x = x, y = y, fill = Sed_class)) + 
-  scale_fill_viridis(name = 'Sediment\nclass (NGU)',  discrete = TRUE) +
+  geom_raster(data = Sed_quant, aes(x = x, y = y, fill = D50)) + 
+  viridis::scale_fill_viridis(name = 'mean grain size (NGU)', trans = "log") +
   theme_minimal() +
-  guides(fill = guide_legend(ncol = 2)) + # Adjustments for poster plotting
   NULL
 toc()
 
