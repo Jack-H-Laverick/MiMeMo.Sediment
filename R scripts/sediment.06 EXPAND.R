44cc2845bff5547358c87c45f45deefcac1ab6c4
Jack H Laverick from Strath <jacklaverick@ymail.com>
2021-03-15 19:37:10 +0000

    feat: implemented {largeList}.

--- R scripts/sediment.06 EXPAND.R
+++ R scripts/sediment.06 EXPAND.R
@@ -3,18 +3,19 @@
 
 rm(list=ls())                                                               # Wipe the brain
 
-packages <- c("MiMeMo.tools", "tidyverse", "tictoc", "furrr", "sf")         # List packages
+packages <- c("MiMeMo.tools", "furrr", "largeList")                         # List packages
 lapply(packages, library, character.only = TRUE)                            # Load packages
 source("./R scripts/@_Set up file.R")
 
-plan(multiprocess)
+plan(multisession)
 
 pairs <- readRDS("./Objects/Water look up table.rds")                       # Import pairings between wave and tide pixels
-pairs_chunked <- split(pairs, (seq(nrow(pairs)) -1) %/% 200)               # Split into chunks of 1000 rows
+pairs_chunked <- split(pairs, (seq(nrow(pairs)) -1) %/% 200)                # Split into chunks of 1000 rows
 chunks <- length(pairs_chunked)
 
-wave_ts <- readRDS("./Objects/Wave_ts.rds")                                 # Read in a list of time series for waves at pixels
+wave_ts <- getList(file = "./Objects/Wave_ts.llo")                          # Connect to out of memory list
 print("loaded waves")
+
 tide_ts <- readRDS("./Objects/Tide_ts.rds")                                 # Read in a list of time series for tides at pixles
 print("loaded tides")
 
@@ -25,7 +26,8 @@ imap(pairs_chunked, ~{ waves <- wave_ts[.x$wave_entry]
                        tides <- tide_ts[.x$tide_entry]
                        map2(waves, tides, cbind) %>%  
   saveRDS(glue::glue("./Objects/stress_input/{.y}.rds"))                  # Then save (allows all the data to be held in memory)
-  print(glue::glue("{.y}/{chunks} chunks"))})
+  print(glue::glue("{.y}/{chunks} chunks"))
+                       }, progress = TRUE)
 toc() 
 
 rm(wave_ts, tide_ts)
